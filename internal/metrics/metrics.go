e5c5b6dc59e08a0440aa98d814a003e56db66d3f
Ochirov Aldar <ochirov-3@mail.ru>
2026-04-28 22:02:05 +0300

    - changed buckets of histogram metric

--- internal/metrics/metrics.go
+++ internal/metrics/metrics.go
@@ -31,7 +31,7 @@ var RequestDurations = prometheus.NewHistogramVec(
 	prometheus.HistogramOpts{
 		Name:    "http_request_duration_seconds",
 		Help:    "request durations",
-		Buckets: []float64{0.001, 0.005, 0.01, 0.05, 0.1, 0.2},
+		Buckets: []float64{0.0001, 0.0005, 0.001, 0.005, 0.01, 0.05, 0.1, 0.2, 0.5},
 	},
 	[]string{"method", "path", "status"},
 )
